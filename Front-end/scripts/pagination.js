74ff5273cc1d2b79afeeb779344b28dd3006bf53
mayur124 <mayur.chawda@somaiya.edu>
2020-09-02 09:41:39 +0530

    pagination changes

--- Front-end/scripts/pagination.js
+++ Front-end/scripts/pagination.js
@@ -8,11 +8,12 @@ export function Pagination(tableElement, pageData, numOfCols) {
         const footerFragment = document.createDocumentFragment();
         const tableFooter = document.createElement("tfoot");
         const th = document.createElement("th");
-        th.classList.add("d-flex", "align-items-center", "justify-content-around");
+        // th.classList.add("d-flex", "align-items-center", "justify-content-around");
         th.append(getFirstPageBtn());
         th.append(getPreviousPageBtn());
         th.append(getNextPageBtn());
         th.append(getLastPageBtn());
+        th.append(getPageDropdown());
         th.colSpan = numOfCols;
         tableFooter.appendChild(th);
         footerFragment.appendChild(tableFooter);
@@ -20,35 +21,56 @@ export function Pagination(tableElement, pageData, numOfCols) {
     }
     function getFirstPageBtn() {
         const btn = getBtn();
-        const firstBtn = btn("&#9668;");
-        firstBtn.addEventListener("click", () => moveTo("first"));
+        const firstBtn = btn("&#9668;&#9668;");
+        setBtnEventTitle(firstBtn, "first", "First");
         return firstBtn;
     }
     function getPreviousPageBtn() {
         const btn = getBtn();
         const prevBtn = btn("&#9664;");
-        prevBtn.addEventListener("click", () => moveTo("previous"));
+        setBtnEventTitle(prevBtn, "previous", "Previous");
         return prevBtn;
     }
     function getNextPageBtn() {
         const btn = getBtn();
         const nextBtn = btn("&#9654;");
-        nextBtn.addEventListener("click", () => moveTo("next"));
+        setBtnEventTitle(nextBtn, "next", "Next");
         return nextBtn;
     }
     function getLastPageBtn() {
         const btn = getBtn();
-        const lastBtn = btn("&#9658;");
-        lastBtn.addEventListener("click", () => moveTo("last"));
+        const lastBtn = btn("&#9658;&#9658;");
+        setBtnEventTitle(lastBtn, "last", "Last");
         return lastBtn;
     }
     function getBtn() {
         return function (hexCode) {
             const btn = document.createElement("button");
             btn.innerHTML = hexCode;
-            btn.classList.add("p-1");
+            btn.className = "mx-1";
             return btn;
         }
+    }
+    /**
+     * 
+     * @param {HTMLButtonElement} btn 
+     * @param {"first"|"previous"|"next"|"last"} moveDirection 
+     * @param {"First"|"Previous"|"Next"|"Last"} title 
+     */
+    function setBtnEventTitle(btn, moveDirection, title) {
+        btn.addEventListener("click", () => moveTo(moveDirection));
+        btn.title = title;
+    }
+    function getPageDropdown() {
+        const pageSizeSelect = document.createElement("select");
+        [5, 10, 25, 50].forEach(pageSize => {
+            let option = new Option(pageSize, pageSize);
+            option.className = "page-size-dropdown";
+            pageSizeSelect.options.add(option);
+        });
+        pageSizeSelect.options.selectedIndex = 0;
+        pageSizeSelect.classList.add("mx-1", "page-size-dropdown");
+        return pageSizeSelect;
     }
     /**
      * 
